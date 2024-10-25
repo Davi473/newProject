113ba9f1310da0a3988f9ab48da1799cb3438404
Davi473 <68740118+Davi473@users.noreply.github.com>
2024-10-25 20:02:19 +0000

    lancamentoModel

--- src/Lancamento/Lancamento.model.ts
+++ src/Lancamento/Lancamento.model.ts
@@ -1,6 +1,50 @@
 import Connection from "../config/dbConnect";
+import Lancamento from "./Lancamento";
+
+interface LancamentoData {
+  id: number;
+  usuario: string;
+  ticket: string;
+  tipo: string;
+  quantidade: number;
+  preco: number;
+  data: Date;
+  compra: boolean;
+}
 
 export default class LancamentoModel
 {
   constructor(private dbConnect: Connection) {}
+
+  public async select(usuario: string)
+  {
+    const lancamentos: LancamentoData[] = await this.dbConnect.query(
+      "SELECT * FROM ativos.lancamento WHERE usuario = $1",
+      [usuario]
+    );
+    return this.mapLancamento(lancamentos);
+  }
+  /*
+  public selectAtivo(usuario: string, ativo: string)
+  {
+    const lancamentos = this.dbConnect.query(
+      ""
+    )
+  }
+  */
+  private mapLancamento(lancamentos: LancamentoData[])
+  {  
+    const lancamento: Lancamento[] = [];
+    if(lancamentos.length > 0)
+    {
+      lancamento.push(...lancamentos.map(lancamento => {
+          return new Lancamento(
+            lancamento.id, lancamento.usuario, lancamento.ticket,
+            lancamento.tipo, lancamento.quantidade, lancamento.preco,
+            lancamento.data, lancamento.compra
+          )
+      }));
+    }
+    return lancamento;
+  }
 }
